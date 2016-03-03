a18add556804501b971bf0f9d79b5f7e9efccba8
arai kentaro <kentaro.arai.08065913704@gmail.com>
2016-03-03 15:17:48 +0900

    add strong parameters to (:account_update)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push(:avatar, :username, :works, :member, :profile)
+    devise_parameter_sanitizer.for(:account_update).push(:avatar, :username, :works, :member, :profile)
   end
 
 end
