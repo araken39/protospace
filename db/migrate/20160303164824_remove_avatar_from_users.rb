a21a9ead438c80cd6d5852df7f8599f5d8ecf6d5
arai kentaro <kentaro.arai.08065913704@gmail.com>
2016-03-04 01:58:32 +0900

    delete unused migration file

--- db/migrate/20160303164824_remove_avatar_from_users.rb
+++
@@ -1,8 +0,0 @@
-class RemoveAvatarFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :avatar_file_name, :string
-    remove_column :users, :avatar_content_type, :string
-    remove_column :users, :avatar_updated_at, :datetime
-    remove_column :users, :avatar_file_size, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303102634) do
+ActiveRecord::Schema.define(version: 20160303164601) do
 
   create_table "captured_images", force: :cascade do |t|
     t.integer  "status",       limit: 1,   default: 0, null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160303102634) do
     t.string   "member",                 limit: 255
     t.string   "profile",                limit: 255
     t.string   "works",                  limit: 255
+    t.string   "avatar",                 limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
