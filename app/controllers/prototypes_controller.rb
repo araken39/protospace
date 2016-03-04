2f82fe38ffd654d3181d7ad53bc593cbd213657b
arai kentaro <kentaro.arai.08065913704@gmail.com>
2016-03-05 01:25:54 +0900

    defined new action

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,8 @@
 class PrototypesController < ApplicationController
   def index
-
+  end
+  def new
+    @prototype = Prototype.new(create_params)
+     3.times { @prototype.captured_images.build }
   end
 end
