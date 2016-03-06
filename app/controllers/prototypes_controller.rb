5c5fc74f970d9138bec58d798451675a22f62fd4
arai kentaro <kentaro.arai.08065913704@gmail.com>
2016-03-06 14:29:58 +0900

    add enum

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,12 +1,27 @@
 class PrototypesController < ApplicationController
+
   def index
   end
+
   def new
     @prototype = Prototype.new
     3.times { @prototype.captured_images.build }
   end
+
+  def create
+    Prototype.create(create_params)
+    redirect_to controller: :prototypes, action: :index
+  end
+
   private
   def create_params
-    params.require(:prototype).permit(captured_images_attributes: [:thumbnail])
+
+    params.require(:prototype).permit(
+        :title,
+        :catch_copy,
+        :concept,
+        captured_images_attributes: [:thumbnail, :status]
+      )
   end
+
 end
