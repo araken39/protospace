f7bca7e2101e81ffbdb461d970e27b43886b573c
arai kentaro <kentaro.arai.08065913704@gmail.com>
2016-03-07 15:35:10 +0900

    add show action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources :users, only: :show
-  resources :prototypes, only: [:new, :create]
+  resources :prototypes, only: [:new, :create, :show]
 end
