3c892a88d9625485fa47dfc2d9fa8d71b14382bd
arai kentaro <kentaro.arai.08065913704@gmail.com>
2016-03-08 17:13:46 +0900

    delete disused method and replace main form find_by

--- app/helpers/prototypes_helper.rb
+++ app/helpers/prototypes_helper.rb
@@ -1,9 +1,7 @@
 
 module PrototypesHelper
+
   def set_main_thumnail(prototype)
-    prototype.captured_images.find_by(status: CapturedImage.statuses[:main]).thumbnail
-  end
-  def set_sub_thumnail(prototype)
-    prototype.captured_images.find_by(status: CapturedImage.statuses[:sub]).thumbnail
+    prototype.captured_images.main.first.thumbnail
   end
 end
