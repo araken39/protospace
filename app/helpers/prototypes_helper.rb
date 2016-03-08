2c10ec0a3a85715bfa56b63b0da1ee29632c484a
arai kentaro <kentaro.arai.08065913704@gmail.com>
2016-03-08 11:02:03 +0900

    modify magic number

--- app/helpers/prototypes_helper.rb
+++ app/helpers/prototypes_helper.rb
@@ -1,9 +1,9 @@
 
 module PrototypesHelper
   def set_main_thumnail(prototype)
-    prototype.captured_images.find_by(status: 0).thumbnail
+    prototype.captured_images.find_by(status: CapturedImage.statuses[:main]).thumbnail
   end
   def set_sub_thumnail(prototype)
-    prototype.captured_images.find_by(status: 1).thumbnail
+    prototype.captured_images.find_by(status: CapturedImage.statuses[:sub]).thumbnail
   end
 end
