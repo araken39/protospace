a07b533386eb08ad5af4a0f37faab1e454ab432d
arai kentaro <kentaro.arai.08065913704@gmail.com>
2016-03-16 07:27:03 +0900

    add edit and destroy action

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
 
-  before_action :set_prototype ,only: :show
+  before_action :set_prototype ,only: [:show, :edit, :destroy, :update]
   def index
     @prototypes = Prototype.all
   end
@@ -12,10 +12,30 @@ class PrototypesController < ApplicationController
 
   def create
     Prototype.create(create_params)
-    redirect_to action: :index
+    redirect
   end
 
   def show
+
+  end
+
+  def edit
+    @prototype.captured_images.build
+  end
+
+  def destroy
+    if @prototype.user_id == current_user.id
+      @prototype.destroy
+    end
+    redirect
+  end
+
+  def update
+    if @prototype.user_id == current_user.id
+      update_params
+      @prototype.update(update_params)
+    end
+    redirect
   end
 
 
@@ -25,11 +45,31 @@ class PrototypesController < ApplicationController
         :title,
         :catch_copy,
         :concept,
-        captured_images_attributes: [:thumbnail, :status]
+        captured_images_attributes: [
+        :thumbnail,
+        :status
+        ]
       ).merge(user_id: current_user.id)
+  end
+
+  def update_params
+    params.require(:prototype).permit(
+      :title,
+      :catch_copy,
+      :concept,
+      captured_images_attributes: [
+      :thumbnail,
+      :status,
+      :id
+      ]
+    )
   end
 
   def set_prototype
     @prototype = Prototype.find(params[:id])
+  end
+
+  def redirect
+    redirect_to action: :index
   end
 end
