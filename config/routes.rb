6da6b38cdadd1917a0300ce6e951f2ff5d57871a
arai kentaro <kentaro.arai.08065913704@gmail.com>
2016-03-16 07:32:58 +0900

    add routes (edit, destroy, upadate)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resources :users, only: :show
-  resources :prototypes, only: [:new, :create, :show]
+
+  resources :users, only: :show do
+    resources :prototypes, only: [:edit, :destroy]
+  end
+
+  resources :prototypes, only: [:new, :create, :show, :update]
 end
