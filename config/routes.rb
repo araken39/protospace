52eb64889722a3c6614e46702fac8943ca01ba92
arai kentaro <kentaro.arai.08065913704@gmail.com>
2016-03-19 08:40:53 +0900

    add  except [: index]   not using  this routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources :users, only: :show do
-    resources :prototypes, except: :show
+    resources :prototypes, except: [:show, :index]
   end
   resources :prototypes, only: :show
 end
