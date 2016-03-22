333db8184f93e4b4956ef4f0741a58b5b1fead43
arai kentaro <kentaro.arai.08065913704@gmail.com>
2016-03-23 06:28:36 +0900

    modify routes not to use nest

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resources :users, only: :show do
-    resources :prototypes, except: [:index]
-  end
+  resources :users, only: :show
+  resources :prototypes
 end
