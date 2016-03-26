d0414518679e8c39d0ff49ebefaf9c5ecb13d21c
arai kentaro <kentaro.arai.08065913704@gmail.com>
2016-03-26 09:57:51 +0900

    rake db:migrate for making likes table and adding like_count to prototypes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306120958) do
+ActiveRecord::Schema.define(version: 20160324175508) do
 
   create_table "captured_images", force: :cascade do |t|
     t.string   "thumbnail",    limit: 255
     t.integer  "status",       limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
     t.integer  "prototype_id", limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -28,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160306120958) do
     t.integer  "user_id",    limit: 4
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.integer  "like_count", limit: 4
   end
 
   create_table "users", force: :cascade do |t|
