7054a5e6f64d9908041121ebbb2160eefcdc7da6
arai kentaro <kentaro.arai.08065913704@gmail.com>
2016-03-26 09:42:47 +0900

    add association to likes

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,10 @@
 class Prototype < ActiveRecord::Base
   has_many :captured_images
+  has_many :likes
   belongs_to :user
   accepts_nested_attributes_for :captured_images
+
+  def like_user(user)
+   likes.find_by(user_id: user.id)
+  end
 end
